32f37a17875552eb531958f003a67d7a4e03bb32
Atarian-ByVoid <cgomespimentel7@gmail.com>
2022-11-11 20:05:04 -0300

    new release

---
+++ src/main/java/br/fepi/ads/qatar/QatarApplication.java
@@ -0,0 +1,13 @@
+package br.fepi.ads.qatar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QatarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QatarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fepi/ads/qatar/QatarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fepi.ads.qatar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QatarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
