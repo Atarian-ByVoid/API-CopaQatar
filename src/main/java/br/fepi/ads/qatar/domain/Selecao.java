29e541d02db7fbffee0ab102babab7d0b5ad6a53
Atarian-ByVoid <cgomespimentel7@gmail.com>
2022-11-11 21:46:03 -0300

    new release

--- src/main/java/br/fepi/ads/qatar/domain/Selecao.java
+++ src/main/java/br/fepi/ads/qatar/domain/Selecao.java
@@ -1,5 +1,66 @@
 package br.fepi.ads.qatar.domain;
 
+import java.sql.Date;
+
 public class selecao {
+
+    private Long id;
+    private boolean favorita;
+    private String cor_camisa_secundaria;
+    private String cor_camisa_principal;
+    private integer numero_copas;
+
+    // id das camisetas 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+
+    }
+
+    //cor da camiseta princial
+    public String setCor_camisa_principal() {
+        return cor_camisa_principal;
+    }
+
+    public void setCor_camisa_principal(String cor_camisa_princial){
+     this.cor_camisa_principal = cor_camisa_principal;
+
+    }
+
+    //cor camiseta secundaria
+    public String setCor_camisa_secundaria(){
+        return cor_camisa_secundaria;
+
+    }
+
+    public void setCor_camisa_secundaria(String cor_camisa_secundaria){
+        this.cor_camisa_secundaria = cor_camisa_secundaria;
+    }
+
+    //camiseta favorita 
+    public boolean setFavorita(){
+        return favorita;
+    }
+
+    public void setFavorita(boolean favorita){
+    this.favorita = favorita;
+    }
+
+    //numero das copas 
+
+    public integer setNumero_copas(){
+        return numero_copas;
+    }
+
+    public void setNumero_copas(integer numero_copas){
+        this.numero_copas=numero_copas;
+    }
     
+
+
+
+    }
 }
